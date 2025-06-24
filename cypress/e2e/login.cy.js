844656ee160f7bd9c9a7d523d20cf4e543cfa95c
yuliaLopa <engineeryulia@gmail.com>
2025-06-24 11:46:19 -0600

    Add more test files and update dependencies

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,21 @@
 ///<reference types = 'cypress' />
+import chai from 'chai';
 import { faker } from '@faker-js/faker';
 import { InputFormPage } from './pages/inputFormPage'
 
 
 const formPage = new InputFormPage
-const randomName = faker.name.fullName()
+const randomName = faker.person.fullName()
+const randomEmail = faker.internet.email()
+const randomPassword = faker.internet.password()
+const randomCompany = faker.company.name()
+const randomWebsite = faker.internet.url()
+const randomCity = faker.location.city()
+const randomAdress1 = faker.location.streetAddress()
+const randomAdress2 = faker.location.secondaryAddress()
+const randomState = faker.location.state()
+const randomZipCode = faker.location.zipCode()
+const country = 'United States';
 
 describe('Input Form Demo', () => {
   it('Page exist', () => {
@@ -13,7 +24,18 @@ describe('Input Form Demo', () => {
     formPage.VerifyTitle();
     formPage.VerifyHeadling();
 
-    // formPage.fillName(randomName)
+    formPage.fillName(randomName)
+    formPage.fillEmail(randomEmail)
+    formPage.fillPassword(randomPassword)
+    formPage.fillCompany(randomCompany)
+    formPage.fillWebsite(randomWebsite)
+    formPage.fillCity(randomCity)
+    formPage.fillAdress1(randomAdress1)
+    formPage.fillAdress2(randomAdress2)
+    formPage.fillState(randomState)
+    formPage.fillZipCode(randomZipCode)
+    formPage.selectCountry(country);
+    formPage.submitButton()
    
   });
 });
--- cypress/e2e/pages/inputFormPage.js
+++ cypress/e2e/pages/inputFormPage.js
@@ -1,3 +1,5 @@
+import { should } from "chai"
+
 export class InputFormPage{
     visit() {
         cy.visit('https://www.lambdatest.com/selenium-playground/input-form-demo')
@@ -15,13 +17,87 @@ export class InputFormPage{
         cy.get('h1').should('be.visible').should('have.text', 'Form Demo')
         cy.get('h2').should('be.visible').should('have.text', 'Input form validations')
     
-
-    // fillName(name){
-    //     cy.get('#name')
-    //     .should('have.attr', 'placeholder', 'Name')
-    //     .should('be.visible')
-    //     .type(name, {force: true})
-    // }
+    }
+    fillName(name){
+        cy.get('input[name="name"]').click().wait(100)
+        .should('have.attr', 'placeholder', 'Name')
+        .should('be.visible')
+        .type(name)
     }
 
+    fillEmail(email){
+        cy.get('input[placeholder="Email"]')
+        .should('have.attr', 'placeholder', 'Email')
+        .should('be.visible')
+        .type(email, {force: true})
+    }
+
+    fillPassword(password){
+        cy.get('input[placeholder="Password"]')
+        .should('have.attr', 'placeholder', 'Password')
+        .should('be.visible')
+        .type(password, {force: true})
+    }
+
+    fillCompany(company){
+        cy.get('#company')
+        .should('have.attr', 'placeholder', 'Company')
+        .should('be.visible')
+        .type(company, {force: true})
+    }
+    
+    fillWebsite(website){
+        cy.get('#websitename')
+        .should('have.attr', 'placeholder', 'Website')
+        .should('be.visible')
+        .type(website, {force: true})
+    }
+
+    fillCity(city){
+        cy.get('#inputCity')
+        .should('have.attr', 'placeholder', 'City')
+        .should('be.visible')
+        .type(city, {force: true} )
+    }
+    
+    fillAdress1(adress1){
+        cy.get('#inputAddress1')
+        .should('have.attr', 'placeholder', 'Address 1')
+        .should('be.visible')
+        .type(adress1, {force: true})
+    }
+
+    fillAdress2(adress2){
+        cy.get('#inputAddress2')
+        .should('have.attr', 'placeholder', 'Address 2')
+        .should('be.visible')
+        .type(adress2, {force: true})
+    }
+
+    fillState(state){
+        cy.get('#inputState')
+        .should('have.attr', 'placeholder', 'State')
+        .should('be.visible')
+        .type(state, {force: true})
+    }
+
+    fillZipCode(zipCode){
+        cy.get('#inputZip')
+        .should('have.attr', 'placeholder', 'Zip code')
+        .should('be.visible')
+        .type(zipCode, {force: true})
+    }
+
+    selectCountry(country) {
+        cy.get('select[name="country"]').first().select('United States');
+          
+        
+    }
+
+    submitButton(button) {
+        cy.get('button[type="submit"]:visible').click()
+        .should('have.length', 1).and('not.be.disabled')
+
+    }
 }
+
